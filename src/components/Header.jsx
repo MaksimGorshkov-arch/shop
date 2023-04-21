f8cd7011c908ed719a81e68b4e8474c09dd97b26
MaksimGorshkov-arch <internetvpomosh@gmail.com>
2023-04-21 13:34:49 +0500

    gh page added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,9 @@ export function Header() {
     return (
         <nav>
             <div className="nav-wrapper grey darken-4">
-                <a href="!#" className="brand-logo right">React Shop</a>
+                <a href="/" className="brand-logo right">React Shop</a>
                 <ul id="nav-mobile" className="left hide-on-med-and-down">
-                    <li><a href="!#">GitHub</a></li>
+                    <li><a href="https://github.com/MaksimGorshkov-arch?tab=repositories" target="_blank" rel="noreferrer">GitHub</a></li>
                 </ul>
             </div>
         </nav>
